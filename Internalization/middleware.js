f19a0dd58abd78dff50c35fbe2859fdfc19c83d2
MD. Peyal Hasan <155246181+peyalhasan@users.noreply.github.com>
2026-01-30 14:31:33 +0600

    Update middleware.js

--- Internalization/middleware.js
+++ Internalization/middleware.js
@@ -1,46 +1,34 @@
-import { match } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
-import { NextResponse } from 'next/server';
-import React from 'react'
+import { NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
 
+import Negotiator from "negotiator";
 
-let defaultLocale = 'en';
 let locales = ['en', 'bn']
+let defaultLocale = 'en'
 
 function getLocale(request) {
-    const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
+    const acceptedLanguage = request.headers.get('accept-language') ?? undefined
 
-    const headers = { 'accept-language': acceptedLanguage };
+    let headers = { 'accept-language': acceptedLanguage };
 
-    const languages = new Negotiator({ headers }).languages();
-
+    let languages = new Negotiator({ headers }).languages()
     return match(languages, locales, defaultLocale)
 }
 
-export default function middleware(request) {
-    // Get the pathname from request url
 
+export function middleware(request) {
     const pathName = request.nextUrl.pathname;
 
-    // Direct path 
-    const pathNameIsMissingLocal = locales.every(locale => !pathName.startsWith(`/${locale}`) && !pathName.startsWith(`/${locale}/`))
+    const pathnameIsMissingLocale = locales.every((locale) => !pathName.startsWith(`/${locale}/`) && pathName !== `/${locale}`)
 
+    if (pathnameIsMissingLocale) {
+        const locale = getLocale(request);
 
-
-    if (pathNameIsMissingLocal) {
-        // detect user's preference & redirect with a locale with a path eg: /en/about
-
-        const locale = getLocale(request)
-
-        return NextResponse.redirect(new URL(`/${locale}${pathName}`, request.url))
+        return NextResponse.redirect(new URL(`/${locale}/${pathName}`, request.url))
     }
-
-
-    return NextResponse.next()
-
 }
 
-
 export const config = {
-    matcher: ["/((?!_next).*)"],
-};
+    matcher: [
+        '/((?!api|assets|.*\\..*|_next).*)',
+    ]
